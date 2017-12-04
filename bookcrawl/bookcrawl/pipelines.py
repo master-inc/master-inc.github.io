c8ff1f9a1d925329b1f6d73a5ea746fb2ee230ea
David Nguyen <34226459+master-inc@users.noreply.github.com>
2017-12-04 13:25:02 +0700

    first trial
    demo

---
+++ bookcrawl/bookcrawl/pipelines.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+import pymongo
+
+from scrapy.conf import settings
+from scrapy.exceptions import DropItem
+from scrapy import log
+
+
+class MongoPipeline(object):
+
+    collection_name = 'books'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGODB_URI'),
+            mongo_db=crawler.settings.get('MONGODB_DB', 'bookzen')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        collection = self.db[self.collection_name]
+        is_valid = True
+        for data in item:
+            if not data:
+                is_valid = False
+                raise DropItem("Missing %s!" % data)
+        if is_valid:
+            if collection.find({'name_unidecode': item['name_unidecode'], 'spider': spider.name}).count() != 0:
+                raise DropItem("Item existed")
+            else:
+                collection.insert({k: v[0] for k, v in dict(item).items()})
+                log.msg("Book added to MongoDB database!", level=log.DEBUG, spider=spider)
+                return item
